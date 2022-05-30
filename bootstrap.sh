6cd8771b49094f9870a7d3b0f0db502a494d6042
Ben Slaughter <benslaughter@github.com>
2022-05-30 12:13:18 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-git pull origin main;
-
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~;
-	echo "This should come here";	
-	source ~/.bash_profile;
-	echo "This should end here";
-}
-
-doIt;
-
-unset doIt;
+source ~/.bash_profile;
